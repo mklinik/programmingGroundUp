11ee9c03f4a3c66976725863ed9daa0418508be0
Markus Klinik <markus.klinik@gmx.de>
2014-02-13 17:33:31 +0100

    power: use local variable

--- power.s
+++ power.s
@@ -8,7 +8,7 @@ _start:
     pushq $3 # second argument: power
     pushq $2 # first argument:  base
     call  power
-    addq  $8, %rsp
+    addq  $8, %rsp # clean up function arguments
 
     # copy return value to ebx
     movl  %eax, %ebx
@@ -21,32 +21,36 @@ _start:
 #
 # variables:
 #    %rdi: remaining power
-#    %rax: current value
-#    %rbx: base value
+#    -8(%rbp): current value
+#    %rax: base value
 
 .type power, @function
 power:
     # function setup
     pushq %rbp
     movq  %rsp, %rbp
+    subq  $8, %rsp # make space for local variable
 
     # initialize index register with power
     movq 24(%rbp), %rdi
 
     # initialize variables
-    movq 16(%rbp), %rax
-    movq 16(%rbp), %rbx
+    movq 16(%rbp), %rax # first argument goes to %rax
+    movq %rax, -8(%rbp) # first argument goes to local var
 
 power_loop:
     cmpq  $1, %rdi
     je    end_power # if power is one, we're done
 
-    imulq %rbx, %rax # multiply, store result in %rax
+    movq 16(%rbp), %rbx # fetch local variable to %rbx
+    imulq %rax, %rbx # multiply, store result in %rbx
+    movq %rbx, 16(%rbp) # store result to local variable
     dec   %rdi
     jmp   power_loop
 
     # function cleanup, return value is in %rax
 end_power:
+    movq 16(%rbp), %rax # copy result to return register
     movq %rbp, %rsp
     popq %rbp
     ret
